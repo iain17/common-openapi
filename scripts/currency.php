421e51ea1570a813f8774c538e293ffe662ac625
Iain Munro <iain17@me.com>
2017-04-16 00:52:39 +0200

    Use ISO 4217

--- scripts/currency.php
+++ scripts/currency.php
@@ -1,12 +1,22 @@
 <?php
     //Written in PHP because its very easy to prototype in and make quick little scripts like this.
     $currencies = array();
-    //http://beautifytools.com/html-to-json-converter.php put the table in there.
+    //http://data.okfn.org/data/core/currency-codes
     $json = file_get_contents("currency.json");
     $rawCurrencies = json_decode($json, true);
     foreach($rawCurrencies as $rawCurrency) {
-        $currencies[] = $rawCurrency['Code'];
+        if($rawCurrency["WithdrawalDate"] != "") {
+            continue;
+        }
+        if($rawCurrency['AlphabeticCode'] == "") {
+            echo "Skipping {$rawCurrency['Entity']} = {$rawCurrency['Currency']}\n";
+            continue;
+        }
+        $currencies[] = $rawCurrency['AlphabeticCode'];
     }
+    //Remove duplicates
+    $currencies = array_values(array_unique($currencies, SORT_STRING));
+    
     //https://www.json2yaml.com/
     file_put_contents("result.json", json_encode($currencies));
 ?>
