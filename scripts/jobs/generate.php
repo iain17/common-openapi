d2a98b747e4dbe4a5dd1d04f371e89e9c1252719
Iain Munro <iain17@me.com>
2017-04-17 12:56:58 +0200

    Added jobs

---
+++ scripts/jobs/generate.php
@@ -0,0 +1,24 @@
+<?php
+    $values = array();
+    $definitions = array();
+    
+    //https://developer.linkedin.com/docs/reference/job-function-codes
+    //+
+    //http://convertjson.com/html-table-to-json.htm
+    $json = file_get_contents("raw.json");
+    $rawCurrencies = json_decode($json, true);
+
+    foreach($rawCurrencies as $rawCurrency) {
+        $code = intval($rawCurrency['FIELD1']);
+        $value = $rawCurrency['FIELD3'];
+    
+        $values[] = $code;
+        $definitions[$code] = $value;
+    }
+    //Remove duplicates
+    $values = array_values(array_unique($values, SORT_STRING));
+    
+    //https://www.json2yaml.com/
+    file_put_contents("values.json", json_encode($values));
+    file_put_contents("../../job_def.json", json_encode($definitions));
+?>
