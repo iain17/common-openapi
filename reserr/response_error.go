5eb24379a6ab675842fe333b105ac6ce5375d4a5
Iain Munro <iain17@me.com>
2017-05-21 15:29:45 +0200

    return multiple errors.

--- reserr/response_error.go
+++ reserr/response_error.go
@@ -10,89 +10,94 @@ import (
 
 type Error struct {
 	Code int32 `json:"code"`
-	Message string `json:"message"`
+	Messages []string `json:"messages"`
 }
 
 
-type UpdateUserByIDDefault struct {
+type ErrorResponse struct {
 	_statusCode int
 	Payload *Error `json:"body,omitempty"`
 }
 
 // By default an unexpected error
-func NewError(statusCode int) *UpdateUserByIDDefault {
+func NewError(statusCode int) *ErrorResponse {
 	if statusCode <= 0 {
 		statusCode = 500
 	}
 
-	return &UpdateUserByIDDefault{
+	return &ErrorResponse{
 		_statusCode: statusCode,
 	}
 }
 
 //The API did something wrong
-func NewUnexpectedError() *UpdateUserByIDDefault {
-	return &UpdateUserByIDDefault{
+func NewUnexpectedError() *ErrorResponse {
+	return &ErrorResponse{
 		_statusCode: 500,
 		Payload: &Error{
 			Code: 500,
-			Message: "Unexpected error",
+			Messages: []string{"Unexpected error"},
 		},
 	}
 }
 
 //The application did something wrong
-func NewAppError(message string) *UpdateUserByIDDefault {
-	return &UpdateUserByIDDefault{
+func NewAppError(message string) *ErrorResponse {
+	return &ErrorResponse{
 		_statusCode: 400,
 		Payload: &Error{
 			Code: 400,
-			Message: message,
+			Messages: []string{message},
 		},
 	}
 }
 
-func NewNonExistenceError(subject string) *UpdateUserByIDDefault {
-	return &UpdateUserByIDDefault{
-		_statusCode: 404,
-		Payload: &Error{
-			Code: 404,
-			Message: subject+" could not be found.",
-		},
-	}
+func NewNonExistenceError(subject string) *ErrorResponse {
+	return NewAppError(subject+" could not be found.").SetCode(404).SetStatusCode(404)
 }
 
-func NewPermissionsError() *UpdateUserByIDDefault {
-	return &UpdateUserByIDDefault{
+func NewPermissionsError() *ErrorResponse {
+	return &ErrorResponse{
 		_statusCode: 401,
 		Payload: &Error{
 			Code: 401,
-			Message: "You are unauthorized to this request",
+			Messages: []string{"You are unauthorized to this request"},
 		},
 	}
 }
 
-// WithStatusCode adds the status to the update user by Id default response
-func (o *UpdateUserByIDDefault) WithStatusCode(code int) *UpdateUserByIDDefault {
+func (o *ErrorResponse) SetStatusCode(code int) *ErrorResponse {
 	o._statusCode = code
 	return o
 }
 
-// SetStatusCode sets the status to the update user by Id default response
-func (o *UpdateUserByIDDefault) SetStatusCode(code int) {
-	o._statusCode = code
-}
-
-// WithPayload adds the payload to the update user by Id default response
-func (o *UpdateUserByIDDefault) SetMessage(message string) *UpdateUserByIDDefault {
+func (o *ErrorResponse) SetMessage(message string) *ErrorResponse {
 	if o.Payload == nil {
 		o.Payload = &Error{}
 	}
-	o.Payload.Message = message
+	o.Payload.Messages = []string{message}
 	return o
 }
 
-func (o *UpdateUserByIDDefault) SetCode(code int32) *UpdateUserByIDDefault {
+func (o *ErrorResponse) SetMessages(messages []string) *ErrorResponse {
+	if o.Payload == nil {
+		o.Payload = &Error{}
+	}
+	o.Payload.Messages = messages
+	return o
+}
+
+func (o *ErrorResponse) AddMessage(message string) *ErrorResponse {
+	if o.Payload == nil {
+		o.Payload = &Error{
+			Messages: []string{},
+		}
+	}
+	o.Payload.Messages = append(o.Payload.Messages, message)
+	return o
+}
+
+func (o *ErrorResponse) SetCode(code int32) *ErrorResponse {
 	if o.Payload == nil {
 		o.Payload = &Error{}
 	}
@@ -101,7 +106,7 @@ func (o *UpdateUserByIDDefault) SetCode(code int32) *UpdateUserByIDDefault {
 }
 
 // WriteResponse to the client
-func (o *UpdateUserByIDDefault) WriteResponse(rw http.ResponseWriter, producer runtime.Producer) {
+func (o *ErrorResponse) WriteResponse(rw http.ResponseWriter, producer runtime.Producer) {
 	rw.WriteHeader(o._statusCode)
 	if o.Payload != nil {
 		payload := o.Payload
